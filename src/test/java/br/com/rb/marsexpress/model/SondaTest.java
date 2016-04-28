097f5becd57292ae9a86253b2df90c064f2277d5
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2016-04-28 00:18:30 -0300

    Incluindo os testes de sonda

---
+++ src/test/java/br/com/rb/marsexpress/model/SondaTest.java
@@ -0,0 +1,42 @@
+package br.com.rb.marsexpress.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.rb.marsexpress.HoustonIHaveAProblemException;
+
+public class SondaTest {
+
+	private static final String SONDA_NOME = "Sonda Protótipo";
+	
+	private Planalto planalto;
+	
+	@Before
+	public void before(){
+		planalto = new Planalto(5, 5);
+	}
+	
+	@Test
+	public void testAterrisar(){
+		Sonda sonda = new Sonda(1, SONDA_NOME);
+		sonda.aterrissar(planalto, new Posicao(1, 2, Direcao.N));
+	}
+	
+	@Test(expected=HoustonIHaveAProblemException.class)
+	public void testFalhaNaAterrissagemX(){
+		Sonda sonda = new Sonda(1, SONDA_NOME);
+		sonda.aterrissar(planalto, new Posicao(6, 1, Direcao.N));
+	}
+	
+	@Test(expected=HoustonIHaveAProblemException.class)
+	public void testFalhaNaAterrissagemY(){
+		Sonda sonda = new Sonda(1, SONDA_NOME);
+		sonda.aterrissar(planalto, new Posicao(1, 6, Direcao.N));
+	}
+	
+	@Test(expected=HoustonIHaveAProblemException.class)
+	public void testFalhaNaAterrissagemXY(){
+		Sonda sonda = new Sonda(1, SONDA_NOME);
+		sonda.aterrissar(planalto, new Posicao(6, 6, Direcao.N));
+	}
+}
