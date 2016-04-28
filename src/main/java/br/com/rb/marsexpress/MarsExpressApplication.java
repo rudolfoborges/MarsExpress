f12bfb530ec1438fead6ef610d8c0a7e2312b744
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2016-04-27 22:03:18 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/rb/marsexpress/MarsExpressApplication.java
@@ -0,0 +1,12 @@
+package br.com.rb.marsexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExpressApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/rb/marsexpress/MarsExpressApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.rb.marsexpress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MarsExpressApplication.class)
+@WebAppConfiguration
+public class MarsExpressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
